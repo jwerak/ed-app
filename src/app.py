9204dd8280922111277ddc1d7300122f156beb6f
Jakub Veverka <veverka.kuba@gmail.com>
2022-04-24 13:19:33 +0200

    Application shows uptime

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from types import FrameType
 import sys
 import os
 import signal
+import datetime
 
 from version import __version__
 
@@ -11,29 +12,42 @@ PORT = int(os.getenv('PORT', 8080))
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'DEV')
 VERSION = __version__
 
-MESSAGE = f'''<html>
-<head><title>Example Application</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-<h1>Hello, World!</h1>
-<p>ENVIRONMENT: <b>{ENVIRONMENT}</b></p>
-<p>VERSION: <b>{VERSION}</b></p>
-</body>
-</html>
-'''
-
 class AppServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        self.wfile.write(bytes(MESSAGE, 'utf8'))
+        msg = getMessage(ENVIRONMENT, VERSION)
+        self.wfile.write(bytes(msg, 'utf8'))
 
 def shutdown_handler(signal: int, frame: FrameType) -> None:
     print('Exiting process.', flush=True)
     sys.exit(0)
+
+def getMessage(env, version):
+    uptime = getUptime() 
+    return f'''<html>
+<head><title>Example Application</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>ENVIRONMENT: <b>{env}</b></p>
+<p>VERSION: <b>{version}</b></p>
+<p>UPTIME: <b>{uptime} seconds</b></p>
+</body>
+</html>
+'''
+
+def getUptime():
+    """
+    Returns the number of seconds since the program started.
+    """
+    uptime = datetime.datetime.now() - startTime
+    return uptime.seconds
+
+startTime = datetime.datetime.now()
 
 if __name__ == '__main__':
     server = HTTPServer((HOST_NAME, PORT), AppServer)
