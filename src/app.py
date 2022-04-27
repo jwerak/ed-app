3c957897879ef614741cf4e824b7ae1f632e6938
jwerak <veverka.kuba@gmail.com>
2022-04-27 19:25:42 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ def shutdown_handler(signal: int, frame: FrameType) -> None:
 def getMessage(env, version):
     uptime = getUptime() 
     return f'''<html>
-<head><title>Example Application</title>
+<head><title>Example Edge Application</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
